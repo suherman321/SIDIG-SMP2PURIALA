8b27df733a02c7f956378cab21522785d8fd63ba
suherman321 <68242161+suherman321@users.noreply.github.com>
2026-08-11 22:22:36 +0800

    apdate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-nilai-v37'; // Naikkan versi cache ke v29
+const CACHE_NAME = 'sidig-v38'; // Naikkan versi cache ke v29
 
 const ASSETS_TO_CACHE = [
   './',
@@ -8,7 +8,6 @@ const ASSETS_TO_CACHE = [
   './manifest.json',
   './logo.png',
   './bg-sekolah.jpg',
-  './card.png'        // <--- Pastikan sesuai dengan nama file gambar Anda
 ];
 
 self.addEventListener('install', (e) => {
@@ -42,10 +41,14 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then((networkResponse) => {
-        if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+        // Izinkan request type 'basic' DAN 'cors' agar link eksternal seperti Google Drive bisa lewat
+        if (networkResponse && networkResponse.status === 200 && (networkResponse.type === 'basic' || networkResponse.type === 'cors')) {
           const responseClone = networkResponse.clone();
           caches.open(CACHE_NAME).then((cache) => {
-            cache.put(e.request, responseClone);
+            // Hindari men-cache extension Chrome yang bikin error kemarin
+            if (!e.request.url.startsWith('chrome-extension')) {
+              cache.put(e.request, responseClone);
+            }
           });
         }
         return networkResponse;
@@ -53,5 +56,7 @@ self.addEventListener('fetch', (e) => {
       .catch(() => {
         return caches.match(e.request);
       })
+  );
+});
   );
 });
